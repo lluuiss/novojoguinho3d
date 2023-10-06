e57efcc44a8ad69360e74291202b321235a9603a
Luiz carlos da silva filho <filho.luiz@academico.ifrn.edu.br>
2023-10-06 16:22:46 -0300

    aula 15 minu 23

--- My project/Assets/scripts/CombateEnemy.cs
+++ My project/Assets/scripts/CombateEnemy.cs
@@ -11,11 +11,16 @@ public class CombateEnemy : MonoBehaviour
     public float attackDamage;
     public float movementSpeed;
     public float lookRadius;
+    public float colliderRadius = 2;
 
     [Header("Components")]
     private Animator anim;
     private CapsuleCollider capsule;
     private NavMeshAgent agent;
+    private bool Walking;
+    private bool attacking;
+    private bool hiting
+    private bool waitFor;
 
     [Header("Others")]
     private Transform player;
@@ -34,22 +39,96 @@ public class CombateEnemy : MonoBehaviour
         float distance = Vector3.Distance(player.position, transform.position);
         if (distance <= lookRadius)
         {
-            agent.SetDestination(player.position);
+            
             //o personagem esta no raio de visao
+            agent.isStopped = false;
+            if (!attacking)
+            {
+                agent.SetDestination(player.position);
+                anim.SetBool("Walk Forward", true);
+                Walking = true;
+            }
             if (distance <= agent.stoppingDistance)
             {
-                Debug.Log("bateu");
+                StartCoroutine("Attack");
+            }
+            else
+            {
+                attacking = false;
             }
         }
         else
         {
            //o personagem esta fora do raio de visao
+           anim.SetBool("Walk Forward", false);
+           agent.isStopped = true;
+           Walking = false;
+           attacking = false;
+        }
+    }
+    
+    
+    IEnumerator Attack()
+    {
+        if (!waitFor)
+        {
+            waitFor = true;
+            attacking = true;
+            Walking = false;
+            anim.SetBool("Walk Forward", false);
+            anim.SetBool("Pounce Attack", true);
+            yield return new WaitForSeconds(1.5f);
+            GetPlayer();
+            //yield return new WaitForSeconds(1f);
+            waitFor = false;
+        }
+        
+        
+    }
+
+    void GetPlayer()
+    {
+        foreach (Collider c in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
+        {
+            if (c.gameObject.CompareTag("Player"))
+            {
+               //vai causar dano no player
+               Debug.Log("varios pulos");
+            }
         }
     }
 
+    public void GetHit(float damage)
+    {
+        totalHealth -= damage;
+        //totalHealth = totalHealth - damage;
+        if (totalHealth > 0)
+        {
+            //esta vivo
+            StopCoroutine("Attack");
+            anim.SetTrigger("Take Damege");
+            hiting = true;
+        }
+        else
+        {
+            //esta morto
+            anim.SetTrigger("Die");
+        }
+    }
+    
+    IEnumerator RecoveryFromHit()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("Walk Forward", false);
+        anim.SetBool("Pounce Attack", true);
+        hiting = false;
+        waitFor = false;
+    }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
+
+
